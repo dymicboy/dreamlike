d495df140b63a0b27239231bad56240d3a69ae63
dkdlqoddi@o365.skku.edu <dkdlqoddi@o365.skku.edu>
2020-05-31 22:33:05 +0900

    Revert "Stage3 진행중"
    This reverts commit d2546fd68d72431fe685b29ba77210b5a5ab4c1a.

--- block.h
+++ block.h
@@ -434,15 +434,7 @@ inline void block_t::update(float t)
 		{
 			rotate_flag = false;
 			if (stage == 3)
-			{
-				sw += 2;
-				if (sw >= 4)
-				{
-					current_theta += PI;
-					target_theta += PI;
-					sw %= 4;
-				}
-			}
+				sw = (sw + 2) % 4;
 		}
 		else {
 			rotate_flag = false;
